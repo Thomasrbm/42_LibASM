c3485c7f3cc8be75d65ed648f2e6ce9c4051801f
Thomasrbm <frozyxyt.76@gmail.com>
2025-12-21 11:41:04 +0100

    feat: strlen

---
+++ ft_strlen.s
@@ -0,0 +1,18 @@
+section .text
+	_start
+
+
+_strlen:
+	xor rax, rax
+
+.while:
+	cmp byte [rdi + rax], 0
+	je .fin_while
+
+	inc rax  ; saut de ligne pour la lisibilite (entre je et inc)
+	jmp .while
+
+
+
+.fin_while:
+	ret
