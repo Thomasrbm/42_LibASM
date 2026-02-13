81e16d195b1cd9ecea2d62b71b7e9a9bac9c3056
Thomasrbm <frozyxyt.76@gmail.com>
2026-02-13 01:27:48 +0100

    Add:  Atoi_base Utf-8 and Utf-32

---
+++ quick_asm/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <uchar.h> // Indispensable pour char32_t
+
+// On déclare ta fonction ASM
+// On utilise long pour correspondre à r14 (64 bits)
+extern long ft_atoi_base_utf32(const char32_t *str, const char32_t *base);
+
+int main() {
+    // Le 'U' magique convertit "↯" (3 octets) en 0x21AF (4 octets)
+    char32_t *string = U"10"; 
+    
+    // Ta base géante (abrégée ici, mais mets tout dedans)
+    char32_t *base = U"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&()*.,/:;<=>?@[]^_`{|}~"
+                     U"¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ"
+					 U"ĀāĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķĸĹĺĻļĽľĿŀŁłŃńŅņŇňŉŊŋŌōŎŏŐőŒœŔŕŖŗŘřŚśŜŝŞşŠšŢţŤťŦŧŨũŪūŬŭŮůŰűŲųŴŵŶŷŸŹźŻżŽž"
+					 U"ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩαβγδεζηθικλμνξοπρστυφχψω"
+					 U"БГДЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюя"
+					 U"←↑→↓↔↕↖↗↘↙↚↛↜↝↞↟↠↡↢↣↤↥↦↧↨↩↪↫↬↭↮↯↰↱↲↳↴↵↶↷↸↹↺↻↼↽↾↿⇀⇁⇂⇃⇄⇅⇆⇇⇈⇉⇊⇋⇌⇍⇎⇏⇐⇑⇒⇓⇔⇕⇖⇗⇘⇙⇚⇛⇜⇞⇟";
+
+
+    long result = ft_atoi_base_utf32(string, base);
+
+    printf("result = %ld\n", result);
+    return 0;
+}
